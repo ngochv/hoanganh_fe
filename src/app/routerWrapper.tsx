54ba7cb23cf95f246f3876a218b585d22368da10
ngochv <hoangngochhnd@gmail.com>
2024-07-24 06:42:12 +0700

    feat: deleted authState when not exits token

--- src/app/routerWrapper.tsx
+++ src/app/routerWrapper.tsx
@@ -1,23 +1,27 @@
 "use client";
 
+import { deleteAuthState } from "@/lib/store/authSlice";
+import { useAppDispatch } from "@/lib/store/store";
 import { isAuthentication } from "@/utils/auth";
 import { usePathname, useRouter } from "next/navigation";
 
 export const RouterWrapper = (props: React.PropsWithChildren<{}>) => {
   const pathname = usePathname();
   const router = useRouter();
+  const dispatch = useAppDispatch();
 
   const notAllowLoggedInPaths = ["/login", "/set-token"];
 
   const invalidPaths = notAllowLoggedInPaths.filter((path: string) =>
     pathname.includes(path)
   );
-  
+
   const isRedirectLogin =
     !!invalidPaths.length ||
     (!invalidPaths.length && !(pathname === "" || pathname === "/"));
 
   if (!isAuthentication() && isRedirectLogin) {
+    dispatch(deleteAuthState());
     router.push("/login");
   }
 
